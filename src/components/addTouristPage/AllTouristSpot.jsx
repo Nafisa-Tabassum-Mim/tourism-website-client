581add0348e0c5ea2198b683677098df25733a2b
Nafisa-Tabassum-Mim <mim.afrin129@gmail.com>
2024-04-28 19:06:06 +0600

    all tourist spot added sorting

--- src/components/addTouristPage/AllTouristSpot.jsx
+++ src/components/addTouristPage/AllTouristSpot.jsx
@@ -1,16 +1,35 @@
+
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 
-
 const AllTouristSpot = () => {
+    const tourisms = useLoaderData();
+    const [sortedTourisms, setSortedTourisms] = useState(tourisms);
 
-    const tourisms = useLoaderData()
-    console.log(tourisms)
+    const sortByCost = (order) => {
+        const sorted = [...tourisms].sort((a, b) => {
+            if (order === "ascending") {
+                return a.averageCost - b.averageCost;
+            } else {
+                return b.averageCost - a.averageCost;
+            }
+        });
+        setSortedTourisms(sorted);
+    };
 
     return (
         <div>
             <div>
-                <p className="text-3xl font-semibold text-center my-8">All Tourists <span className="text-blue-400"> Spots</span></p>
-                {tourisms.map((tourism) => (
+                <p className="text-3xl font-semibold text-center my-8">All Tourists <span className="text-blue-400 mr-2"> Spots</span>
+                    <div className="dropdown dropdown-hover">
+                        <div tabIndex={0} role="button" className="btn m-1 text-white bg-blue-400 text-xl">Sort</div>
+                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a onClick={() => sortByCost("ascending")}>Cost - low to high </a></li>
+                            <li><a onClick={() => sortByCost("descending")}>Cost - high to low</a></li>
+                        </ul>
+                    </div>
+                </p>
+                {sortedTourisms.map((tourism) => (
                     <div key={tourism._id}>
                         <div className="card md:card-side shadow-sm border shadow-blue-300 my-4 mx-4">
                             <figure>
@@ -19,7 +38,6 @@ const AllTouristSpot = () => {
                             <div className="mx-4">
                                 <h1 className="text-xl font-bold md:mt-8 mb-2">{tourism.tourismName}</h1>
 
-                         
                                 <div className="flex border-b-2 pb-2 items-center font-semibold">
                                     <p className="flex items-center mr-8 gap-2 ">  Visitors per year - {tourism.totalVisitorsPerYear} </p>
                                     <p className="flex items-center gap-2">Best Season - {tourism.seasonality} </p>
@@ -30,7 +48,6 @@ const AllTouristSpot = () => {
                                 </div>
                                 <div className="my-2">
                                     <button className="text-[#328EFF] rounded-2xl bg-[#328EFF26] font-semibold px-4 mr-2 mb-2 py-[2px]">Location - {tourism.location} </button>
-                              
                                 </div>
                                 <div>
                                     <Link to={`/${tourism._id}`} className="btn mb-2 bg-blue-500 font-semibold text-white"> View Details</Link>
@@ -40,7 +57,6 @@ const AllTouristSpot = () => {
                     </div>
                 ))}
             </div>
-            {/* </div>) */}
         </div >
     );
 };
